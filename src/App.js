a7d1b356ab7ad0387c4bfad60c1cf7532ea0b251
woo-jk <201702042@o.cnu.ac.kr>
2022-09-30 23:05:30 +0900

    프로젝트 기초공사: 폰트 세팅, 기본 레이아웃 세팅, 이미지 에셋 세팅

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import RouterTest from "./components/RouterTest";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouterTest />
       </div>
     </BrowserRouter>
   );

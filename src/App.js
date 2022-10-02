51ee2845180b35239cff986dcee7f6de9762252d
woo-jk <201702042@o.cnu.ac.kr>
2022-10-02 23:21:46 +0900

    프로젝트 기초공사1 : 공통 컴포넌트생성

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"hi"}
+          leftChild={
+            <MyButton text={"왼쪽"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton text={"오른쪽"} onClick={() => alert("오른쪽 클릭")} />
+          }
+        />
         <h2>App.js</h2>
-        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <MyButton text="버튼" onClick={() => alert("클릭")} type="positive" />
+        <MyButton text="버튼" onClick={() => alert("클릭")} />
+        <MyButton text="버튼" onClick={() => alert("클릭")} type="negative" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
